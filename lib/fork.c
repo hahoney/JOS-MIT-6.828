9b66d537661361071d28ec7f65d572c54fc828f5
Yi Zhang <hahoney@gmail.com>
2014-12-11 00:26:56 -0800

    Lab4

---
+++ lib/fork.c
@@ -0,0 +1,163 @@
+// implement fork from user space
+
+#include <inc/string.h>
+#include <inc/lib.h>
+
+// PTE_COW marks copy-on-write page table entries.
+// It is one of the bits explicitly allocated to user processes (PTE_AVAIL).
+#define PTE_COW		0x800
+
+//
+// Custom page fault handler - if faulting page is copy-on-write,
+// map in our own private writable copy.
+//
+static void
+pgfault(struct UTrapframe *utf)
+{
+	void *addr = (void *) utf->utf_fault_va;
+	uint32_t err = utf->utf_err;
+	int r;
+
+	// Check that the faulting access was (1) a write, and (2) to a
+	// copy-on-write page.  If not, panic.
+	// Hint:
+	//   Use the read-only page table mappings at uvpt
+	//   (see <inc/memlayout.h>).
+
+	// LAB 4: Your code here.
+
+        if (!(err & FEC_WR) && !(uvpt[(uint32_t) addr >> PTXSHIFT] & PTE_COW)) {
+           panic("The pgfault can not be handled due to access limit\n");
+        }
+
+	// Allocate a new page, map it at a temporary location (PFTEMP),
+	// copy the data from the old page to the new page, then move the new
+	// page to the old page's address.
+	// Hint:
+	//   You should make three system calls.
+
+	// LAB 4: Your code here.
+        // PTE_COW is not necessary since it is the state to trig pgfault handling
+        if ((r = sys_page_alloc(0, (void *)PFTEMP, PTE_U|PTE_W|PTE_P)) < 0) {
+           panic("sys_page_alloc(): allocation failed\n");
+        }
+
+        uint32_t alignaddr = ROUNDDOWN((uint32_t)addr, PGSIZE);
+
+        memmove((void *)PFTEMP, (void *)alignaddr, PGSIZE);
+
+        r = sys_page_map(0, (void *)PFTEMP, 0, (void *)alignaddr, PTE_W|PTE_U|PTE_P);
+        if (r < 0) {
+           panic("Can not map new page to PFTEMP\n");
+        }
+ 
+	//panic("pgfault not implemented");
+}
+
+//
+// Map our virtual page pn (address pn*PGSIZE) into the target envid
+// at the same virtual address.  If the page is writable or copy-on-write,
+// the new mapping must be created copy-on-write, and then our mapping must be
+// marked copy-on-write as well.  (Exercise: Why do we need to mark ours
+// copy-on-write again if it was already copy-on-write at the beginning of
+// this function?)
+//
+// Returns: 0 on success, < 0 on error.
+// It is also OK to panic on error.
+//
+static int
+duppage(envid_t envid, unsigned pn)
+{
+	int r;
+        void *addr = (void *) (pn * PGSIZE);
+
+	// LAB 4: Your code here.
+        if ((uvpt[pn] & PTE_W) || (uvpt[pn] & PTE_COW)) {
+            r = sys_page_map(0, addr,
+                 envid, addr, PTE_COW|PTE_P|PTE_U);
+            if (r < 0) { return -1; }
+
+            // self mapping to replace the old page
+            r = sys_page_map(0, (void *) (pn * PGSIZE),
+                 0, (void *) (pn * PGSIZE), PTE_COW|PTE_P|PTE_U);
+            if (r < 0) { return -1; }
+        }
+        // otherwise set the page unwritable
+        else {
+            r = sys_page_map(0, (void *) (pn * PGSIZE),
+                 envid, (void *) (pn * PGSIZE), PTE_P|PTE_U);
+            if (r < 0) { return -1; }
+        }
+
+	//panic("duppage not implemented");
+	return 0;
+}
+
+//
+// User-level fork with copy-on-write.
+// Set up our page fault handler appropriately.
+// Create a child.
+// Copy our address space and page fault handler setup to the child.
+// Then mark the child as runnable and return.
+//
+// Returns: child's envid to the parent, 0 to the child, < 0 on error.
+// It is also OK to panic on error.
+//
+// Hint:
+//   Use uvpd, uvpt, and duppage.
+//   Remember to fix "thisenv" in the child process.
+//   Neither user exception stack should ever be marked copy-on-write,
+//   so you must allocate a new page for the child's user exception stack.
+//
+envid_t
+fork(void)
+{
+	// LAB 4: Your code here.
+        struct Env *childenv;
+        int envid;
+        int addr;
+        int r;
+
+        // also do memory allocation for parent UXSTACK.
+        set_pgfault_handler(pgfault);
+
+        // we are the child
+        envid = sys_exofork();
+        if (envid < 0) { panic("sys_exofork: %e", envid); }
+        if (envid == 0) {
+            thisenv = &envs[ENVX(sys_getenvid())];
+            return 0;
+        }
+
+        // We are the parent
+        for (addr = UTEXT; addr < UTOP - PGSIZE; addr+=PGSIZE) {
+            if ((uvpd[PDX(addr)] & PTE_P) && (uvpt[PGNUM(addr)] & PTE_P) && (uvpt[PGNUM(addr)] & PTE_U)) {
+                if ((r = duppage(envid, PGNUM(addr))) < 0) {
+                    panic("duppage(): duppage failed\n");
+                }  
+            }                   
+        }
+
+        // // Allocate a new exception stack.
+        r = sys_page_alloc(envid, (void *) (UXSTACKTOP - PGSIZE), PTE_P|PTE_W|PTE_U);
+        if (r < 0) { panic("env page allocation failed\n"); }
+
+        // get the child env
+        extern void _pgfault_upcall(void);
+        sys_env_set_pgfault_upcall(envid, (void *) _pgfault_upcall);
+      
+        if ((r = sys_env_set_status(envid, ENV_RUNNABLE)) < 0) {
+                panic("sys_env_set_status: %e", r);
+        }
+
+        return envid;
+	//panic("fork not implemented");
+}
+
+// Challenge!
+int
+sfork(void)
+{
+	panic("sfork not implemented");
+	return -E_INVAL;
+}
